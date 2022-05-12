1019c9e2e2181320de0d5bf15a3c9be0a5648315
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2022-05-12 19:16:18 +0200

    A bad string hasher

--- hasher.go
+++ hasher.go
@@ -1,5 +1,7 @@
 package hashtree
 
+import "math/bits"
+
 // The Hasher type provides a way to hash values as well as compare them for
 // equality.
 //
@@ -15,8 +17,32 @@ type Numeric interface {
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
 }
 
-type NumericHasher[T Numeric] struct{}
+type numericHasher[T Numeric] struct{}
 
-func (NumericHasher[T]) Equal(a, b T) bool { return a == b }
-func (NumericHasher[T]) Hash(a T) uint64   { return uint64(a) }
+func (numericHasher[T]) Equal(a, b T) bool { return a == b }
+func (numericHasher[T]) Hash(a T) uint64   { return uint64(a) }
 
+/* Once this works (in Go 1.19 maybe) the hasher implementations can be
+   used directly instead of going through the "constructor" functions
+func f[T any](h Hasher[T]) {}
+var _ = func() struct{} {
+	f(NumericHasher[int]{})
+	return struct{}{}
+}()
+*/
+
+func NumericHasher[T Numeric]() Hasher[T] { return numericHasher[T]{} }
+
+type stringHasher[T ~string] struct{}
+
+func (stringHasher[T]) Equal(a, b T) bool { return a == b }
+func (stringHasher[T]) Hash(a T) (res uint64) {
+	// TODO: This is bad because the two halves of the output are
+	//  formed independently by the even and odd characters...
+	for _, c := range a {
+		res = bits.RotateLeft64(res, 32) ^ uint64(c)
+	}
+	return res
+}
+
+func StringHasher[T ~string]() Hasher[T] { return stringHasher[T]{} }
